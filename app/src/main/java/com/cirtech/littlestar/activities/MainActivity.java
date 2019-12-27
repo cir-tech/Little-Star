45f69bcbb12958264b9c032591908e4c1527c019
zone@inatto.com <zone@inatto.com>
2019-12-27 18:42:01 -0300

    Initial commit

---
+++ app/src/main/java/com/cirtech/littlestar/activities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cirtech.littlestar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cirtech.littlestar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botaoPratica = (Button) findViewById(R.id.idPratica);
+        Button botaoTeorica = (Button) findViewById(R.id.idTeorico);
+
+        botaoPratica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePage = new Intent(MainActivity.this,PraticaActivity.class);
+                startActivity(changePage);
+            }
+        });
+
+        botaoTeorica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePage = new Intent(MainActivity.this,TeoricaActivity.class);
+                startActivity(changePage);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/cirtech/littlestar/activities/PraticaActivity.java
@@ -0,0 +1,58 @@
+package com.cirtech.littlestar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cirtech.littlestar.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class PraticaActivity extends AppCompatActivity {
+
+    private TextView txvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pratica);
+        txvResult = (TextView) findViewById(R.id.idTxtResult);
+    }
+
+    public void getSpeechInput(View view) {
+
+//        String texto ="ola";
+//        Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 10) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                txvResult.setText(result.get(0));
+            }
+        } else {
+            throw new IllegalStateException("Unexpected value: " + requestCode);
+        }
+    }
+}
