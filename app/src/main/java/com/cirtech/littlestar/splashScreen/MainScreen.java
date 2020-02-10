46ffbf751afe638caeb5c09015f044f97486c410
Iuriaraujocs <iuriaraujoc.eng@gmail.com>
2020-02-10 11:34:39 -0300

    - colocou-se um icon de voltar na atividade prática. - alterou-se a cor de fundo do splash screen para se adequar com a foto

--- app/src/main/java/com/cirtech/littlestar/splashScreen/MainScreen.java
+++ app/src/main/java/com/cirtech/littlestar/splashScreen/MainScreen.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.cirtech.littlestar.R;
 import com.cirtech.littlestar.activities.MainActivity;
@@ -15,6 +17,8 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_main_screen);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
