d4e12d4323b9879d6c560f101544178e86f23333
kiofelipe <caio_f.cardoso@hotmail.com>
2020-03-22 04:40:02 -0300

    Estrela 3 adicionado Login finalizado

--- app/src/main/java/com/cirtech/littlestar/activities/MainActivity.java
+++ app/src/main/java/com/cirtech/littlestar/activities/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_opcoes);
 
         Button botaoPratica =  findViewById(R.id.idPratica);
         Button botaoTeorica =  findViewById(R.id.idTeorico);
