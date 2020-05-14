ff82e65ef4aa053ecebd0a1846f7c039be2db655
Iuriaraujocs <iuriaraujoc.eng@gmail.com>
2020-05-13 22:09:34 -0300

    - adicionando a parte pratica das cores

---
+++ app/src/main/java/com/cirtech/littlestar/activities/PracticeColorsActivity.java
@@ -0,0 +1,14 @@
+package com.cirtech.littlestar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticeColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_colors);
+    }
+}
