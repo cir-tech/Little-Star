e5fc4394a7b0c74a5936fd9ee0a261b32beaed66
Iuriaraujocs <iuriaraujoc.eng@gmail.com>
2020-05-26 21:26:37 -0300

    - adicionando pratica de animais e cores

---
+++ app/src/main/java/com/cirtech/littlestar/PracticeAnimalsColorsActivity.java
@@ -0,0 +1,14 @@
+package com.cirtech.littlestar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticeAnimalsColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_animals_colors);
+    }
+}
