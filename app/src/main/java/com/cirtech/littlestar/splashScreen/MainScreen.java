2c12e8ea45bc030e06c3dc04e77c104961908068
Iuriaraujocs <iuriaraujoc.eng@gmail.com>
2020-05-28 20:06:05 -0300

    - versao preliminar com novo layout compilando

--- app/src/main/java/com/cirtech/littlestar/splashScreen/MainScreen.java
+++ app/src/main/java/com/cirtech/littlestar/splashScreen/MainScreen.java
@@ -1,40 +1,40 @@
-package com.cirtech.littlestar.splashScreen;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.Window;
-import android.view.WindowManager;
-
-import com.cirtech.littlestar.R;
-import com.cirtech.littlestar.activities.LoginActivity;
-import com.cirtech.littlestar.activities.MainActivity;
-
-public class MainScreen extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen_main_screen);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                mostrarMainActivity();
-            }
-        }, 1500);
-    }
-
-    private void mostrarMainActivity() {
-        Intent intent = new Intent(
-                MainScreen.this, LoginActivity.class
-        );
-        startActivity(intent);
-        finish();
-    }
-}
+//package com.cirtech.littlestar.splashScreen;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.os.Handler;
+//import android.view.Window;
+//import android.view.WindowManager;
+//
+//import com.cirtech.littlestar.R;
+//import com.cirtech.littlestar.activities.LoginActivity;
+//import com.cirtech.littlestar.activities.MainActivity;
+//
+//public class MainScreen extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.splash_screen_main_screen);
+////        requestWindowFeature(Window.FEATURE_NO_TITLE);
+////        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+//        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                mostrarMainActivity();
+//            }
+//        }, 1500);
+//    }
+//
+//    private void mostrarMainActivity() {
+//        Intent intent = new Intent(
+//                MainScreen.this, LoginActivity.class
+//        );
+//        startActivity(intent);
+//        finish();
+//    }
+//}
