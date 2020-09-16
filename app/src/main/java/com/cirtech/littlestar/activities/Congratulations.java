cc364e26f60b7bce1531cb1f8ea7f56d47170f13
kiofelipe <caio_f.cardoso@hotmail.com>
2020-09-16 13:43:56 -0300

    Camera adicionada com o botão da Screenshot (ainda com o problema de tela preta)

--- app/src/main/java/com/cirtech/littlestar/activities/Congratulations.java
+++ app/src/main/java/com/cirtech/littlestar/activities/Congratulations.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,13 +31,23 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Congratulations extends AppCompatActivity {
+    android.hardware.Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congratulations);
+        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+        //abrindo a camera
+        camera = Camera.open(1);
+        showCamera = new ShowCamera(this,camera);
+        frameLayout.addView(showCamera);
+
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
         }
@@ -45,6 +57,7 @@ public class Congratulations extends AppCompatActivity {
         view1.setDrawingCacheEnabled(true);
         Bitmap bitmap =  Bitmap.createBitmap(view1.getDrawingCache());
         view1.setDrawingCacheEnabled(false);
+
 
         String filePath = Environment.getExternalStorageDirectory()+ "/Download/"+ Calendar.getInstance().getTime().toString()+".png";
         File fileScreenshot = new File(filePath);
@@ -67,7 +80,6 @@ public class Congratulations extends AppCompatActivity {
             this.startActivity(intent);
 
         }
-
 
 
 
