5bf668c2dec195eb30657a660a99f854ff599971
kiofelipe <caio_f.cardoso@hotmail.com>
2020-09-16 22:39:17 -0300

    Camera adicionada com o botão da Screenshot (ainda com o problema de tela preta)

--- app/src/main/java/com/cirtech/littlestar/activities/Congratulations.java
+++ app/src/main/java/com/cirtech/littlestar/activities/Congratulations.java
@@ -1,10 +1,12 @@
 package com.cirtech.littlestar.activities;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.hardware.Camera;
 import android.net.Uri;
@@ -15,9 +17,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.ContentResolver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
@@ -31,42 +38,84 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Congratulations extends AppCompatActivity {
-    android.hardware.Camera camera;
-    FrameLayout frameLayout;
-    ShowCamera showCamera;
+    public static final int CAMERA_PERM_CODE = 101;
+    //    android.hardware.Camera camera;
+//    FrameLayout frameLayout;
+//    ShowCamera showCamera;
+    ImageView selectedImage;
+    Button cameraBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congratulations);
-        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
+//        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
 
         //abrindo a camera
-        camera = Camera.open(1);
-        showCamera = new ShowCamera(this,camera);
-        frameLayout.addView(showCamera);
+//        camera = Camera.open(1);
+//        showCamera = new ShowCamera(this,camera);
+//        frameLayout.addView(showCamera);
+
+        //CAMERA
+        selectedImage = findViewById(R.id.displayImageView);
+        cameraBtn = findViewById(R.id.cameraBtn);
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermissions();
+            }
+        });
 
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+        //SCREENSHOT
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
         }
 
-        public void ScreenshotButton(View view){
+    private void askCameraPermissions() {
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+
+        }else {
+            openCamera();
+        }
+
+        public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+                 if (requestCode == CAMERA_PERM_CODE){
+                     if (grantResults.length > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED ){
+                     // abre a camera
+                     }else{
+                         Toast.makeText(this, "Voce precisa permitir o uso da camera para continuar!", Toast.LENGTH_SHORT).show();
+                     }
+                 }
+        }
+
+    }
+
+    private void openCamera() {
+        Toast.makeText(this, "solicitação de abertura de câmera", Toast.LENGTH_SHORT);
+
+    }
+
+
+    public void ScreenshotButton(View view){
         View view1 = getWindow().getDecorView().getRootView();
         view1.setDrawingCacheEnabled(true);
         Bitmap bitmap =  Bitmap.createBitmap(view1.getDrawingCache());
         view1.setDrawingCacheEnabled(false);
 
 
-        String filePath = Environment.getExternalStorageDirectory()+ "/Download/"+ Calendar.getInstance().getTime().toString()+".png";
+        String filePath = Environment.getExternalStorageDirectory()+ "/Download/"+ Calendar.getInstance().getTime().toString()+".jpeg";
         File fileScreenshot = new File(filePath);
 
         FileOutputStream fileOutputStream = null;
 
             try {
                 fileOutputStream = new FileOutputStream(fileScreenshot);
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
                 fileOutputStream.flush();
                 fileOutputStream.close();
             } catch (Exception e) {
