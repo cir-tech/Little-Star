67e7404ea7d0e6095bf8748a0a5c3c46e998ca69
Iuriaraujocs <iuriaraujoc.eng@gmail.com>
2020-10-28 23:19:54 -0300

    - login funcional de forma assincrona

--- app/src/main/java/com/cirtech/littlestar/activities/LoginActivity.java
+++ app/src/main/java/com/cirtech/littlestar/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.cirtech.littlestar.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.view.ViewCompat;
@@ -8,69 +9,107 @@ import android.content.SharedPreferences;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.cirtech.littlestar.R;
 import com.cirtech.littlestar.services.AuthenticateUser;
+import com.cirtech.littlestar.util.GsonUtil;
+
+import java.util.Map;
 
 
 //import com.cirtech.littlestar.splashScreen.MainScreen;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements AuthenticateUser.GetJSONListener{
 
     private static final String PREFERENCE = "preferencesAppInfo";
 
     private ImageView logo;
+    private ImageView btnLogar;
+    private EditText login;
+    private EditText password;
+
+    private ProgressBar loadSpinner;
+
+    @Override
+    public void onRemoteCallComplete(String response,String status){
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE,0); //0 é preferencia privada
+        // a qual apenas nosso app pode ver
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if(!status.equals("ok")){
+            Log.i("resultado do servidor",response);
+            loadSpinner.setVisibility(View.GONE);
+            this.ImageViewEnable(btnLogar,true);
+            Toast.makeText(LoginActivity.this,"O login falhou, tente novamente", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Map dataResponse = GsonUtil.json_decode(response);
+
+            Log.i("resultado do servidor",dataResponse.get("status").toString());
+            loadSpinner.setVisibility(View.GONE);
+            this.ImageViewEnable(btnLogar,true);
+
+            if(dataResponse.get("status").toString().equals("success")){
+                Toast.makeText(LoginActivity.this,"Logado com sucesso!", Toast.LENGTH_SHORT).show();
+                editor.putString("logado","logado");
+                 editor.apply();   //commit()
+                redirecionar();
+            }else Toast.makeText(LoginActivity.this,"O login falhou, tente novamente", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ImageView btnLogar = findViewById(R.id.idbtnLogar);
         logo = findViewById(R.id.idLogoLogin);
+        btnLogar = findViewById(R.id.idbtnLogar);
+        login = findViewById(R.id.idlogin);
+        password = findViewById(R.id.idpassword);
+        loadSpinner = findViewById(R.id.idloadlogin);
 
-        btnLogar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logarApp();
-            }
-        });
+
+        btnLogar.setOnClickListener(view -> logarApp());
 
         redirecionaIfLogou();
     }
 
-
-//
     private void logarApp(){
-        EditText login = findViewById(R.id.idlogin);
-        EditText password = findViewById(R.id.idpassword);
 
-        new AuthenticateUser().execute();
+        String loginString = this.login.getText().toString();
+        String passwordString = this.password.getText().toString();
 
-        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE,0); //0 é preferencia privada
-        // a qual apenas nosso app pode ver
-        SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        String loginString = login.getText().toString();
-        String passwordString = password.getText().toString();
-
-        if(loginString.equals("") || passwordString.equals("")){
-            Toast.makeText(LoginActivity.this,"Por favor, preencher corretamente", Toast.LENGTH_SHORT).show();
-        }else if(loginString.equals("desenv") && passwordString.equals("12345") ){
-            Toast.makeText(LoginActivity.this,"Logado com sucesso!", Toast.LENGTH_SHORT).show();
-            editor.putString("logado","logado");
-//            SharedPreferences.Editor remove (String key)
-//            SharedPreferences.Editor.clear()
-            editor.apply();   //commit()
-            redirecionar();
-        }else {
-            Toast.makeText(LoginActivity.this,"Login ou senha incorreto!", Toast.LENGTH_SHORT).show();
+        if(loginString.equals("") || passwordString.equals("")) {
+            Toast.makeText(LoginActivity.this, "Por favor, preencher corretamente", Toast.LENGTH_SHORT).show();
         }
+        else {
+            this.loadSpinner.setVisibility(View.VISIBLE);
+            this.ImageViewEnable(btnLogar,false);
+            new AuthenticateUser(loginString,passwordString).execute(this);
+        }
+
+//        if(loginString.equals("") || passwordString.equals("")){
+//            Toast.makeText(LoginActivity.this,"Por favor, preencher corretamente", Toast.LENGTH_SHORT).show();
+//        }else if(loginString.equals("desenv") && passwordString.equals("12345") ){
+//            Toast.makeText(LoginActivity.this,"Logado com sucesso!", Toast.LENGTH_SHORT).show();
+//            editor.putString("logado","logado");
+////            SharedPreferences.Editor remove (String key)
+////            SharedPreferences.Editor.clear()
+//            editor.apply();   //commit()
+//            redirecionar();
+//        }else {
+//            Toast.makeText(LoginActivity.this,"Login ou senha incorreto!", Toast.LENGTH_SHORT).show();
+//        }
 
     }
 
@@ -92,9 +131,9 @@ public class LoginActivity extends AppCompatActivity {
                 redirecionar();
             }
         }
-        else{
-            Toast.makeText(LoginActivity.this,"ja nao foi logado", Toast.LENGTH_SHORT).show();
-        }
+//        else{
+//            Toast.makeText(LoginActivity.this,"ja nao foi logado", Toast.LENGTH_SHORT).show();
+//        }
     }
 
     public void openBrowser(View view){
@@ -110,5 +149,14 @@ public class LoginActivity extends AppCompatActivity {
         intent.setData(Uri.parse(url));
 
         startActivity(intent);
+    }
+
+    private void ImageViewEnable(ImageView imageView, Boolean choice){
+                imageView.setEnabled(choice);
+                if(choice){
+                    imageView.setImageAlpha(255);
+                }
+                else imageView.setImageAlpha(100);
+
     }
 }
--- app/src/main/java/com/cirtech/littlestar/services/AuthenticateUser.java
+++ app/src/main/java/com/cirtech/littlestar/services/AuthenticateUser.java
@@ -2,8 +2,13 @@ package com.cirtech.littlestar.services;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.cirtech.littlestar.comunications.ServiceApi;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import org.json.JSONObject;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -11,12 +16,25 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-public class AuthenticateUser extends AsyncTask<Void, Void, Void> {
+public class AuthenticateUser {
 
-    @Override
-    protected Void doInBackground(Void... voids) {
+    private String email;
+    private String password;
+
+    private GetJSONListener getJSONListener;
+
+    public AuthenticateUser(String email, String password){
+
+        this.email =  email;
+        this.password = password;
+    }
+
+    public String execute(GetJSONListener getJSONListener) {
         final  String url = "http://ec2-18-221-185-30.us-east-2.compute.amazonaws.com";
-        Log.i("teste login auth","passou inBackground loginzao");
+//        Log.i("teste login auth","passou inBackground loginzao");
+
+
+        this.getJSONListener = getJSONListener;
 
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(ScalarsConverterFactory.create())
@@ -25,18 +43,23 @@ public class AuthenticateUser extends AsyncTask<Void, Void, Void> {
 
         ServiceApi service = retrofit.create(ServiceApi.class);
 
-        Call<String> stringCall = service.authenticate("iuri5@gmail.com","12345");
+        Call<String> stringCall = service.authenticate(this.email,this.password);
 
-        stringCall.enqueue(new Callback<String>() {
+         stringCall.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
                 if (response.isSuccessful()) {
+
                     String responseString = response.body();
-                    Log.i("resposta_string",responseString);
-//                        Log.i("resposta_string","deu certo");
+                    Log.i("result","login efetuado com sucesso");
+                    getJSONListener.onRemoteCallComplete(responseString,"ok");
                 }
                 else {
-                    Log.i("resposta_string","nao deu certo");
+
+                    String responseString = response.message();
+//                    JSONObject jObjError = new JSONObject(response.errorBody().string());
+//                    String responseString = jObjError.getJSONObject("error").getString("message");
+                    getJSONListener.onRemoteCallComplete(responseString,"error");
                 }
 
             }
@@ -45,8 +68,23 @@ public class AuthenticateUser extends AsyncTask<Void, Void, Void> {
             public void onFailure(Call<String> call, Throwable t) {
                 String responseString = t.toString();
                 Log.i("resposta_string",responseString);
+//                getJSONListener.onRemoteCallComplete(responseString,"error");
             }
         });
         return null;
     }
+
+
+    public interface GetJSONListener {
+        public void onRemoteCallComplete(String jsonFromNet,String statusResponse);
+    }
+
+    public class APIError {
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+    }
 }
+
